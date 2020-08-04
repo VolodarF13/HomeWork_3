edf8fc586ac3348d85abbca8cf1c671dc89ee3c0
Vova <volodiabodnarchyk2000@gmail.com>
2020-08-04 20:05:41 +0300

    start

---
+++ script.js
@@ -0,0 +1,73 @@
+// "use Strict";
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello JavaScript");
+// console.log(num);
+
+
+ // console.log(calc(4, 3));
+ // console.log(calc(5, 6));
+ // console.log(calc(10, 6));
+
+
+ // function calc(a, b) {
+ //     return (a + b);
+ // }
+
+
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+
+// const anotherNum = ret ();
+// console.log(anotherNum);
+
+
+
+// const logger = function() {
+//     console.log("Hello my friends");
+// };
+
+// logger();
+
+// const calc = (a, b) => { 
+//     console.log('1');
+//     return a + b;
+// };
+
+// const str = "text";
+//  const arr = [1, 2, 4]; 
+
+//  console.log(arr.length);
+
+//  console.log(str.toUpperCase());
+//  console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit"));
+
+// const logg = "Hello Worldex";
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+// console.log(logg.substr(6, 5));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
